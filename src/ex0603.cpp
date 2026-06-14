c40f304b8373ae595059dabb93fdfaa0a02e5bbf
Rhonda Perez Atencio <rperezatencio@mail.weber.edu>
2026-06-14 04:52:25 +0000

    done with week 6

---
+++ src/ex0603.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+enum class BillKind {
+    ONE, FIVE, TEN, TWENTY
+};
+
+// Corrected typo: 'struc' to 'struct'
+struct CashInOneBillKind {
+    BillKind kind;
+    int count;
+};
+
+struct Cash {
+    CashInOneBillKind ones;
+    CashInOneBillKind fives;
+    CashInOneBillKind tens;
+    CashInOneBillKind twenties;
+};
+
+int totalCash(Cash& c) {
+    return (
+        c.ones.count * 1 +
+        c.fives.count * 5 +
+        c.tens.count * 10 +
+        c.twenties.count * 20
+    );
+}
+
+int totalCash2(Cash* c) {
+    return (
+        c->ones.count * 1 +
+        c->fives.count * 5 +
+        c->tens.count * 10 +
+        c->twenties.count * 20
+    );
+}
+
+int main() {
+    Cash inWallet {
+        {BillKind::ONE, 2},
+        {BillKind::FIVE, 3},
+        {BillKind::TEN, 1},
+        {BillKind::TWENTY, 3}
+    };
+
+    cout << "Total cash in wallet: " << totalCash(inWallet) << endl;
+    cout << "Total cash in wallet: " << totalCash2(&inWallet) << endl;
+
+    Cash *dwallet = new Cash {
+        {BillKind::ONE, 2},
+        {BillKind::FIVE, 3},
+        {BillKind::TEN, 1},
+        {BillKind::TWENTY, 3}
+    };
+
+    cout << "Total cash in wallet: " << totalCash(*dwallet) << endl;
+    cout << "Total cash in wallet: " << totalCash2(dwallet) << endl;
+
+
+    return 0;
+}
